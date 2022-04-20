33567de2ceb9a152dbcb660a781db2a7a04de46a
Salenblock <obrazcov.aleksander@gmail.com>
2022-04-20 11:03:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+<template>
+<div class="app">
+  <img class="logo" alt="Vue logo" src="./assets/logo.png">
+  
+<h1 class="h1">Simon the GAME</h1>
+<p>Запомни и открой все зеленные клетки</p>
+
+<BoardBoard />
+</div>
+</template>
+
+<script>
+import BoardBoard from './components/BoardBoard';
+  export default {
+    name: 'App',
+    components: {
+      BoardBoard,
+
+    }
+  }
+</script>
+
+<style scoped>
+.app {
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+}
+.logo {
+  width: 100px;
+}
+</style>
